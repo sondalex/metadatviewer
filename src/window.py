4ac65f9e7e484fdb6f54ad3d3606b75c87a01db6
sondalex <61547150+sondalex@users.noreply.github.com>
2022-06-24 13:23:53 +0200

    Add Table preview

--- src/window.py
+++ src/window.py
@@ -16,7 +16,13 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from gi.repository import Gtk, Gio
+
+# GObject
 import pyreadstat
+import datetime
+
+import numpy as np
+import pandas as pd
 
 # import logging
 
@@ -27,6 +33,29 @@ CSS = b"""
         font-size: large;
         }
         """
+
+
+def convert_pandas(data_frame: pd.DataFrame):
+    """Convert pandas to python native for compatibility with Gtk"""
+    columns = data_frame.columns
+    # detect NaN columns
+    for col in columns:
+        condition = data_frame[col].isna().any()
+        if condition == True:
+            data_frame[col] = data_frame[col].replace({np.nan, "NaN"})
+            # logging.warning("REPLACED")
+            data_frame[col] = data_frame[col].astype(str)
+        data_frame[col] = data_frame[col].astype("object")
+        if (
+            isinstance(
+                data_frame.iloc[0, data_frame.columns.get_loc(col)],
+                (datetime.datetime, datetime.date, datetime.time),
+            )
+            is True
+        ):
+            # conversion for compatibility with Gtk Types.
+            data_frame[col] = data_frame[col].astype(str)
+    return data_frame
 
 
 @Gtk.Template(resource_path="/io/github/sondalex/MetaDatViewer/window.ui")
@@ -62,7 +91,9 @@ class ViewMetaWindow(Gtk.ApplicationWindow):
             self.box.remove(self.box.get_last_child())
             self.box.remove(self.box.get_last_child())
 
-        _, dta_metadata = pyreadstat.read_dta(filepath, metadataonly=True)
+        df, dta_metadata = pyreadstat.read_dta(
+            filepath, metadataonly=False, row_offset=0, row_limit=10
+        )
         column_labels = dta_metadata.column_names_to_labels
         number_rows = dta_metadata.number_rows
         number_columns = dta_metadata.number_columns
@@ -76,8 +107,32 @@ class ViewMetaWindow(Gtk.ApplicationWindow):
         column_labels_label.set_markup("<b>Column-Labels</b>")
         column_labels_box.append(column_labels_label)
 
+        df_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        df_box.set_spacing(10)
+        df_label = Gtk.Label()
+        df_label.set_markup("<b> Data Preview (First 10 rows). </b>")
+        df_box.append(df_label)
+
+        df = convert_pandas(df)
+
+        python_types = [int] + [
+            type(df.iloc[0, df.columns.get_loc(col)]) for col in df.columns
+        ]
+        # logging.warning(python_types)
+        df_table = Gtk.ListStore(*python_types)
+        for row in df.itertuples():
+            df_table.append(tuple(row))
+
+        df_treeview = Gtk.TreeView.new_with_model(df_table)
+
+        for i, column in enumerate(["index"] + df.columns.tolist()):
+            renderer = Gtk.CellRendererText()
+            column = Gtk.TreeViewColumn(column, renderer, text=i)
+            df_treeview.append_column(column)
+
         for tuple_ in variable_info:
             self.table.append(list(tuple_))
+
         self.treeview = Gtk.TreeView.new_with_model(self.table)
         for i, column_title in enumerate(["Column", "Label"]):
             renderer = Gtk.CellRendererText()
@@ -106,8 +161,14 @@ class ViewMetaWindow(Gtk.ApplicationWindow):
         self.scrollable_treelist.set_child(self.treeview)
         column_labels_box.append(self.scrollable_treelist)
 
+        df_scrollable = Gtk.ScrolledWindow()
+        df_scrollable.set_vexpand(True)
+        df_scrollable.set_child(df_treeview)
+        df_box.append(df_scrollable)
+
         self.box.append(summary_stat)
         self.box.append(column_labels_box)
+        self.box.append(df_box)
 
     def open_file_dialog(self, action, _):
         self._native = Gtk.FileChooserNative(
